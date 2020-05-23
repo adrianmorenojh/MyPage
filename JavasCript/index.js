7caf6ef474319821b6c600576f3acbdb9bc7fcda
adrianmh <adrianmorenojh@hotmail.com>
2020-05-23 11:46:28 -0500

    proyecto finalizado

--- JavasCript/index.js
+++ JavasCript/index.js
@@ -15,25 +15,25 @@ var Pelis=
     '../public/images/pelis/pelis3.png'
     ];
 var count = 0;
-
+var countPelis = 0;
 function rotation(){
     count ++
-    if(count === 4){
+    if(count === 5){
         count=0;
     }
     document.getElementById("image").src=DrInk[count];
 }
 function rotationPelis(){
-    count ++
-    if(count === 2){
-        count=0;
+    countPelis ++
+    if(countPelis === 3){
+        countPelis=0;
     }
-    document.getElementById("imagePelis").src=Pelis[count];
-    debugger
+    document.getElementById("imagePelis").src=Pelis[countPelis];
 }
 
 onload=function(){
-
+    rotationPelis()
+    rotation()
     setInterval(() => {
         rotationPelis()
         rotation()
